df3e13cf9b8163e6801ec68857c2059e3cfa7cd7
Praharsh <praharsh3004@gmail.com>
2024-08-06 01:06:49 +0530

    graph intro

---
+++ Graphs/Graph_Adjacency_List/GraphNode.java
@@ -0,0 +1,13 @@
+package Graphs.Graph_Adjacency_List;
+import java.util.*;
+public class GraphNode {
+    public String name;
+    public int index;
+
+    public ArrayList<GraphNode> neighbors = new ArrayList<GraphNode>();
+
+    public GraphNode(String name, int index) {
+        this.name = name;
+        this.index = index;
+    }
+}
---
+++ Graphs/Graph_Adjacency_Matrix/GraphNode.java
@@ -0,0 +1,11 @@
+package Graphs.Graph_Adjacency_Matrix;
+
+public class GraphNode {
+    public String name;
+    public int index;
+
+    public GraphNode(String name, int index) {
+        this.name = name;
+        this.index = index;
+    }
+}
