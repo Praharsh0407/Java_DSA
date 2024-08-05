35870bab952ab17f0fb2c80276eb6d76280da4bd
Praharsh <praharsh3004@gmail.com>
2024-08-05 21:20:58 +0530

    ok

--- Backtracking/N_Queens.java
+++ Backtracking/N_Queens.java
@@ -9,6 +9,37 @@ public class N_Queens {
         this.N = N; //sets the class instance variable N with the given input N to represent the board's size
     }
 
+    private void printQueens(){
+        for(int i = 0;i<N;i++){
+            for(int j = 0;j<N;j++){
+                if(queens[i] ==j){
+                    System.out.print("Q ");
+                }
+                else{
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void placeNQueens(){ //initiate placing of queens on board
+        if (placeQueen(0)){
+            printQueens();
+        }
+        else{
+            System.out.println("No Solution found");
+        }
+    }
+    private boolean canPlace(int row, int col){
+        for(int i = 0;i<col;i++){
+            if(queens[i] == row|| Math.abs(queens[i]-row) == Math.abs(i-col)){ //checks if there's any queen in the same row or diagonals that threatens the current cell.
+                return false; //Queen cant be placed here
+            }
+        }
+        return true;
+    }
+
     private boolean placeQueen(int col){
         if (col == N){
             return true; //all queens placed successfully
@@ -26,40 +57,10 @@ public class N_Queens {
         return false; //no valid placement found in current column
     }
 
-    private boolean canPlace(int row, int col){
-        for(int i = 0;i<col;i++){
-            if(queens[i] == row|| Math.abs(queens[i]-row) == Math.abs(i-col)){ //checks if there's any queen in the same row or diagonals that threatens the current cell.
-                return false; //Queen cant be placed here
-            }
-        }
-        return true;
-    }
-
-    public void placeNQueens(){ //initiate placing of queens on board
-        if (placeQueen(0)){
-            printQueens();
-        }
-        else{
-            System.out.println("No Solution found");
-        }
-    }
-
-    private void printQueens(){
-        for(int i = 0;i<N;i++){
-            for(int j = 0;j<N;j++){
-                if(queens[i] ==j){
-                    System.out.print("Q ");
-                }
-                else{
-                    System.out.print("* ");
-                }
-            }
-            System.out.println();
-        }
-    }
+    
 
     public static void main(String[] args) {
-        N_Queens nQueens = new N_Queens(10);
+        N_Queens nQueens = new N_Queens(4);
         nQueens.placeNQueens();
     }
 }
